352f7fe00970ccf2bdf9852195843c7db77f030f
Jinbe2543 <wuttipat.opor@gmail.com>
2023-09-14 00:43:56 +0700

    draf one

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Homenormal from "./homeNormal/Homenormal";
+import Layout from "./layout";
+
+function App() {
+  return (
+    <>
+      <Layout />
+      <Homenormal />
+    </>
+  );
+}
+
+export default App;
